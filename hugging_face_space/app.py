a8fbc715998ffb654d2f74280116d3a30aa50b8d
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-14 14:32:13 -0500

    new model test

--- hugging_face_space/app.py
+++ hugging_face_space/app.py
@@ -3,7 +3,7 @@ from huggingface_hub import InferenceClient
 import random
 import time
 
-client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
+client = InferenceClient("deepseek-ai/DeepSeek-R1-Zero")
 def respond(
     message,
     history: list[tuple[str, str]],
