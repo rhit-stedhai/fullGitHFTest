e30b6c584c8b4832c52ad9f860aa0c0491b20bc7
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-14 14:20:34 -0500

    second succes test hopefully

--- hugging_face_space/app.py
+++ hugging_face_space/app.py
@@ -44,7 +44,7 @@ css_string = """
 """
 
 # use js and css parameter when changing the look of the window on load
-#with gr.Blocks(css) as demo:
+# with gr.Blocks(css) as demo:
 demo = gr.ChatInterface(
     respond,
     css=css_string,
@@ -53,28 +53,4 @@ demo = gr.ChatInterface(
 if __name__ == "__main__":
     demo.launch()
 
-
-    # additional_inputs=[
-    #     gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
-    #     gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-    #     gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-    #     gr.Slider(
-    #         minimum=0.1,
-    #         maximum=1.0,
-    #         value=0.95,
-    #         step=0.05,
-    #         label="Top-p (nucleus sampling)",
-    #     ),
-    # ],
-
-    ######
-    # run action locally and test to see if it is doing what i actually think it is, do later if script fails, this is annoying to do
-    # 
-    # make a script to accomplish what the action is doing
-    # - clone the repo, copy certain files, and push to the space repo
-    # - would happen everytime we push to github
-    # 
-    # wild way:
-    # - configure a master repo, our repo mimics it and push to their repo directly
-    # - local checkout can have multiple origin repos, change which one it pushes to
-    # - would have to be identical
+    
