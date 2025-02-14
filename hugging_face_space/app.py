4ffef352c1762471a93a0e470e7f16850b7cd0e0
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-14 15:20:50 -0500

    llama 8B testibg

--- hugging_face_space/app.py
+++ hugging_face_space/app.py
@@ -3,7 +3,7 @@ from huggingface_hub import InferenceClient
 import random
 import time
 
-client = InferenceClient("HuggingFaceH4/mistral-7b-sft-beta")
+client = InferenceClient("meta-llama/Llama-3.1-8B")
 def respond(
     message,
     history: list[tuple[str, str]],
