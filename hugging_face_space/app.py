653286654ac5a023b7131b95199fcb14ed3fa7e6
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-14 15:05:10 -0500

    switching back to zephyr

--- hugging_face_space/app.py
+++ hugging_face_space/app.py
@@ -3,7 +3,7 @@ from huggingface_hub import InferenceClient
 import random
 import time
 
-client = InferenceClient("openai-community/gpt2")
+client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
 def respond(
     message,
     history: list[tuple[str, str]],
