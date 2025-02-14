b259e0a7f28747ddce404cb7780b3b5d728b97ac
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-14 15:56:02 -0500

    back to zephyr

--- hugging_face_space/app.py
+++ hugging_face_space/app.py
@@ -3,7 +3,7 @@ from huggingface_hub import InferenceClient
 import random
 import time
 
-client = InferenceClient("deepseek-ai/DeepSeek-R1")
+client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
 def respond(
     message,
     history: list[tuple[str, str]],
