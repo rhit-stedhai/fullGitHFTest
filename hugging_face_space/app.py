d4e010290906e7ca76acd2cdb4d856a2d0d5d7f9
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-14 15:10:07 -0500

    tiny llama

--- hugging_face_space/app.py
+++ hugging_face_space/app.py
@@ -3,7 +3,7 @@ from huggingface_hub import InferenceClient
 import random
 import time
 
-client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
+client = InferenceClient("HuggingFaceH4/tiny-random-LlamaForCausalLM")
 def respond(
     message,
     history: list[tuple[str, str]],
