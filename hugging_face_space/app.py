74f18f173e4641a46d302c1fbcd88e54ad4f9549
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-15 16:55:23 -0500

    switch to filepath

--- hugging_face_space/app.py
+++ hugging_face_space/app.py
@@ -81,7 +81,7 @@ css_string = """
 with gr.Blocks() as demo:
     chatbot = gr.ChatInterface(respond, 
                                css=css_string,
-                               additional_inputs = [gr.File(label="Upload a text file", type="file")],
+                               additional_inputs = [gr.File(label="Upload a text file", type="filepath")],
                                )
     save_button = gr.Button("Save Chat")
     file_output = gr.File()
