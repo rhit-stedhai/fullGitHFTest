7199d5f1a740bbee0868d11f36788f323db7ebdb
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-16 14:44:57 -0500

    launch

--- hugging_face_space/app.py
+++ hugging_face_space/app.py
@@ -66,10 +66,10 @@ app.add_middleware(
 @app.get("/api/chat/")
 async def chat_get():
     return {"chat history": chat_history}
-    
+
 gradioApp = gr.mount_gradio_app(app, demo, path="/")
 
 if __name__ == "__main__":
-    #demo.launch()
+    demo.launch()
     uvicorn.run(gradioApp, host="0.0.0.0", port=7860)
 
